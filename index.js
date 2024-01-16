e06d4bf8d61ff8fbfd9f6cb042674d51152e7a3d
Hisbelis Vargas <hisbelisv@gmail.com>
2024-01-16 16:47:53 -0600

    show completed tasks filter

--- index.js
+++ index.js
@@ -1,23 +1,65 @@
-var taskInput = document.getElementById('taskInput');
-var addTask = document.getElementById('addTask');
-var taskList = document.getElementById('taskList');
+const taskInput = document.getElementById('taskInput');
+const addTaskButton = document.getElementById('addTask');
+const taskList = document.getElementById('taskList');
+const showCompletedCheckbox = document.getElementById('showCompleted');
 
-addTask.addEventListener('click', addNewTask);
+addTaskButton.addEventListener('click', addTask);
 
-function addNewTask() {
+showCompletedCheckbox.addEventListener('change', () => renderTasks());
+
+const tasks = [];
+//(text, completed, deleted)
+
+function addTask() {
     //get the user input
-    var taskName = taskInput.value.trim();
-    if (taskName === '') return;
-    //create the element that represent the task
-    var li = document.createElement('li');
-    li.innerHTML = '<input type="checkbox">' + '<span class="task">' + taskName + '</span>' + '<button class="delete">Delete</button>'
+    const taskText = taskInput.value.trim();
+    if (taskText === '') return;
+    //create new task
+    const newTask = {text: taskText, completed: false, deleted: false};
+    tasks.push(newTask);
 
-    var deleteButton = li.querySelector('.delete');
-    deleteButton.addEventListener('click', () => {
-        li.remove();
-    })
-    //render this task in the task list.
-    taskList.appendChild(li);
     //reset the value in the input
     taskInput.value = '';
+    console.log(tasks);
+   renderTasks();
+}
+
+function renderTasks() {
+    console.log(tasks);
+    // reset the task list
+    taskList.innerHTML= '';
+    const showCompleted = showCompletedCheckbox.checked;
+    //iterate over the tasks array
+    for (let i = 0; i< tasks.length; i++) {
+        const {text, completed, deleted} = tasks[i];
+        if(!deleted && (showCompleted || !completed)) {
+        // create list item
+            const li = document.createElement('li');
+        //checkbox + span + button
+            li.innerHTML = `
+                <input type="checkbox" ${completed ? 'checked' : ''}>
+                <span class="task ${completed ? 'completed' : ''}">${text}</span>
+                <button class="delete">Delete</button>
+            `
+            //add event listeners
+            const deleteButton = li.querySelector('.delete');
+            deleteButton.addEventListener('click', () => deleteTask(i));
+
+            const checkbox = li.querySelector("input[type='checkbox']");
+            checkbox.addEventListener('change', () => toggleCompletion(i));
+            //append li
+            taskList.appendChild(li);
+        }
+    } 
+}
+
+function deleteTask(index) {
+    tasks[index].deleted = true;
+    renderTasks();
+
+}
+
+function toggleCompletion(index) {
+    tasks[index].completed = !tasks[index].completed;
+    renderTasks();
 }
